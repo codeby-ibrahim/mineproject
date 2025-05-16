fe70ca6837db86534a6aa1028c04acb8fbb20211
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com.com>
2025-05-16 15:26:39 +0500

    commit_first

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const input = document.querySelector('input');
 
 input.addEventListener('input', (e) => {
-  const inputvalue = e.target.value
-  if(inputvalue.length === 4 )
-  console.log(e.target.value)
-})
+  const inputValue = e.target.value;
+  console.log(inputValue);
+
+  if (inputValue.length === 4) {
+    const areaCode = '234'.substring(0, 2); // "23"
+    const lastDigit = inputValue[inputValue.length - 1];
+    input.value = `+(${areaCode})- ${lastDigit}`;
+  }
+});
